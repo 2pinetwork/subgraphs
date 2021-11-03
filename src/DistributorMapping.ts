7a284779f5ba4b39ef38e382eedd344a831f6400
Gwydce <gwydce@2pi.network>
2021-11-03 12:58:49 -0300

    KPI working (Missing LP prices)

--- src/DistributorMapping.ts
+++ src/DistributorMapping.ts
@@ -1,4 +1,4 @@
-import { Address, BigDecimal, BigInt, ethereum } from "@graphprotocol/graph-ts"
+import { Address, BigDecimal, BigInt, ValueKind, ethereum } from "@graphprotocol/graph-ts"
 import { FoundersDistributed, } from '../generated/Distributor/Distributor'
 import { IChainlink } from '../generated/Distributor/IChainlink'
 import { KPI } from '../generated/schema'
@@ -16,12 +16,14 @@ export function handleKPI(event: FoundersDistributed): void {
     kpi = new KPI(event.block.timestamp.toString())
   }
 
-  kpi.marketCap = bundle.marketCap
+  kpi.marketCap = bundle.marketCap.truncate(4)
   kpi.holders = bundle.holdersCount
   kpi.transactions = bundle.deposits.plus(bundle.withdraws)
   kpi.totalTVL = bundle.stakedTVL.plus(
     getTotalTVL()
-  )
+  ).truncate(4)
+  kpi.timestamp = event.block.timestamp
+  kpi.score = calcKPIScore(kpi).truncate(6)
 
   kpi.save()
 }
@@ -36,24 +38,69 @@ function getTotalTVL(): BigDecimal {
   }
 
   for (let i = BigInt.fromI32(0); i.lt(poolCall.value); i = i.plus(BigInt.fromI32(1))) {
-    let poolInfo = IArchimedes.poolInfo(i)
-    let oracleAddr: Address | null = ChainLinkOracles.get(poolInfo.value0)
-
-    if (oracleAddr === null) {
-      continue
-    }
-
-    let oracle = IChainlink.bind(oracleAddr)
-    let price = oracle.latestRoundData().value1.toBigDecimal().div(
-      BigDecimal.fromString(oracle.decimals().toString())
-    )
-
-    amount = amount.plus(
-      IArchimedes.balance(i).toBigDecimal().times(price).div(
-        BigDecimal.fromString(IArchimedes.decimals(i).toString())
-      )
-    )
+    amount = amount.plus(getPriceFromPool(i))
   }
 
   return amount
+}
+
+function getPriceFromPool(i: BigInt): BigDecimal {
+  let poolInfo = IArchimedes.poolInfo(i)
+  let oracleAddr: Address | null = ChainLinkOracles.get(poolInfo.value0)
+
+  if (oracleAddr === null) {
+    return getLPPrice(i, poolInfo.value0)
+  } else {
+    return getPriceFromChainlink(i, oracleAddr)
+  }
+}
+
+function getPriceFromChainlink(i: BigInt, oracleAddr: Address): BigDecimal {
+  let oracle = IChainlink.bind(oracleAddr)
+  let oraclePrecision =  BigDecimal.fromString('1' + '0'.repeat(oracle.decimals()))
+  let price = oracle.latestRoundData().value1.toBigDecimal().div(
+    oraclePrecision
+  )
+
+  let poolPrecision = BigDecimal.fromString('1' + '0'.repeat(IArchimedes.decimals(i).toI32()))
+
+  return IArchimedes.balance(i).toBigDecimal().times(price).div(poolPrecision)
+}
+
+function getLPPrice(i: BigInt, lpAddr: Address): BigDecimal {
+  return BigDecimal.fromString('0')
+}
+
+function calcKPIScore(kpi: KPI): BigDecimal {
+  let score = BigDecimal.fromString('0')
+
+  // This should change/improve for the next tranches
+  let kpiData = new Map<string,Array<BigDecimal>>()
+  kpiData.set('totalTVL',     [BigDecimal.fromString('0.4'), BigDecimal.fromString('10000000')]) // metric: [weight, expected]
+  kpiData.set('marketCap',    [BigDecimal.fromString('0.4'), BigDecimal.fromString('15000000')])
+  kpiData.set('holders',      [BigDecimal.fromString('0.1'), BigDecimal.fromString('2000')])
+  kpiData.set('transactions', [BigDecimal.fromString('0.1'), BigDecimal.fromString('5000')])
+
+  const METRICS = ['totalTVL', 'marketCap', 'holders', 'transactions']
+
+  for (let i = 0; i < METRICS.length; i++) {
+    let metric = METRICS[i]
+    let kpiD = kpiData.get(metric)
+    let kpiValue = kpi.get(metric)!
+    let kpiBD: BigDecimal
+
+    if (kpiValue.kind == ValueKind.BIGDECIMAL) {
+      kpiBD = kpiValue.toBigDecimal()
+    } else {
+      kpiBD = kpiValue.toBigInt().toBigDecimal()
+    }
+
+    let rate = kpiBD.times(kpiD[0]).div(kpiD[1])
+
+    if (rate.gt(kpiD[0])){ rate = kpiD[0] }
+
+    score = score.plus(rate)
+  }
+
+  return score
 }
--- src/PiTokenMapping.ts
+++ src/PiTokenMapping.ts
@@ -36,48 +36,36 @@ export function handleBurn(event: Burned): void {
 }
 
 function recordEvent(event: ethereum.Event): void {
-  log.info("LA CONCHA DE TU HNA 1", [])
   let token = new PiToken(idForEvent(event))
-  log.info("LA CONCHA DE TU HNA 2", [])
   let bundle = getBundle()
-  log.info("LA CONCHA DE TU HNA 3", [])
 
   token.totalSupply = IPiToken.totalSupply()
   token.totalStaked = IPiToken.balanceOf(STK_PI_ADDRESS)
 
-  log.info("LA CONCHA DE TU HNA 4", [])
   if (event.block.number > BigInt.fromString('45')) {
     bundle.priceUSD = piPrice().truncate(4)
   } else {
     bundle.priceUSD = BigDecimal.fromString('0.08') // initial price
   }
 
-  log.info("LA CONCHA DE TU HNA 5", [])
   token.priceUSD = bundle.priceUSD
   token.holders = bundle.holdersCount
 
-  log.info("LA CONCHA DE TU HNA 6", [])
   token.marketCap = token.totalSupply.toBigDecimal().times(
     token.priceUSD
   ).div(BIG_DECIMAL_1E18).truncate(4)
 
-  log.info("LA CONCHA DE TU HNA 7", [])
   token.stakedTVL = token.totalStaked.toBigDecimal().times(
     token.priceUSD
   ).div(BIG_DECIMAL_1E18).truncate(4)
 
-  log.info("LA CONCHA DE TU HNA 8", [])
   token.timestamp = event.block.timestamp
 
-  log.info("LA CONCHA DE TU HNA 9", [])
-  // bundle.marketCap = token.marketCap
-  // bundle.stakedTVL = token.stakedTVL
+  bundle.marketCap = token.marketCap
+  bundle.stakedTVL = token.stakedTVL
 
-  log.info("LA CONCHA DE TU HNA 10", [])
   bundle.save()
-  log.info("LA CONCHA DE TU HNA 11", [])
   token.save()
-  log.info("LA CONCHA DE TU HNA 12", [])
 }
 
 export function ethPerPi(): BigDecimal {
